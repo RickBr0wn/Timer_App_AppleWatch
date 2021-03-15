0e4d075a478c82f130aec3349fec43c5b71d3b6c
Rick Brown <ricky.brown.00@gmail.com>
2021-03-15 09:49:15 +0000

    Initial Commit

---
+++ Timer_App_AppleWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer_App_AppleWatch WatchKit Extension
+//
+//  Created by Rick Brown on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timer_App_AppleWatch WatchKit Extension/Timer_App_AppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  Timer_App_AppleWatchApp.swift
+//  Timer_App_AppleWatch WatchKit Extension
+//
+//  Created by Rick Brown on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Timer_App_AppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
